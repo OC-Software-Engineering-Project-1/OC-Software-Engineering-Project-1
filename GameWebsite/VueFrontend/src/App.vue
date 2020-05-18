284b7deb2a58ff91676a674beb92c7750f2f1b92
Diego Vazquez <diegovaz33@gmail.com>
2020-05-18 14:59:54 -0500

    Initial Commit (Login mechanics)

--- GameWebsite/VueFrontend/src/App.vue
+++ GameWebsite/VueFrontend/src/App.vue
@@ -1,45 +1,6 @@
 <template>
-    <div id="app">
-        <p>
-            <router-link :to="{ name: 'home', params: { title: 'Board Game Website' }}">Home</router-link>
-            <br/>
-            <router-link :to="{ name: 'gamelobbies', params: { lobbyList: lobbyListData }}">Game Lobbies</router-link>
-        </p>
-        <router-view />
-    </div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
-
-<script src="https://unpkg.com/vue/dist/vue.js"></script>
-<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
-<script>
-    import Home from './components/Home.vue';
-    import GameLobbies from './components/GameLobbies.vue';
-
-    export default {
-        name: 'app',
-        components: {
-            Home,
-            GameLobbies
-        },
-        data() {
-            return {
-                lobbyListData: [
-                    {
-                        "gameTitle": "Cryptogram Game",
-                        "occupancy": "1",
-                        "capacity": "2"
-                    },
-                    {
-                        "gameTitle": "Kintsugi",
-                        "occupancy": "3",
-                        "capacity": "4"
-                    }
-                ]
-            }
-        }
-    };
-</script>
-
-<style>
-</style>
 
---
+++ GameWebsite/VueFrontend/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import GameLobbies from "../views/GameLobbies.vue";
+
+Vue.use(VueRouter);
+const routes = [
+  {
+    path: "/home",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/gamelobbies",
+    name: "gamelobbies",
+    component: GameLobbies
+  },
+  {
+    path: "/",
+    name: "login",
+    component: () => import("../views/login.vue")
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("../views/register.vue")
+  }
+];
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+export default router;
