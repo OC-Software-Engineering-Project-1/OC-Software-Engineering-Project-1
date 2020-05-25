7e8230b6eccf4c98fdde27d56147bc76c0fb7026
JamesEHannah <35670901+JamesEHannah@users.noreply.github.com>
2020-05-25 02:44:03 -0500

    Fixed navbar bug and added better styling.

--- GameWebsite/VueFrontend/src/App.vue
+++ GameWebsite/VueFrontend/src/App.vue
@@ -1,9 +1,29 @@
 <template>
     <div id="app">
+        <div class="hidden">
+          <Navigation v-show="shouldShowNav"></Navigation>
+        </div>
         <router-view />
     </div>
 </template>
 
 <style>
+[v-cloak].hidden {
+   display: none;
+}
 </style>
 
+<script>
+import Navigation from './components/Navigation'
+export default {
+  name: 'app',
+  components: {
+    'Navigation': Navigation
+  },
+  computed: {
+    shouldShowNav() {
+      return !(this.$route.name === null || this.$route.name === 'login' || this.$route.name === 'register');
+    }
+  }
+}
+</script>
---
+++ GameWebsite/VueFrontend/src/components/Navigation.vue
@@ -0,0 +1,76 @@
+// Credits to Maegan Wilson (source: https://medium.com/@maeganwilson_/how-to-create-a-navigation-bar-in-vue-js-8a70e7f29f80)
+<template>
+    <div class="navbar"> 
+      <nav> 
+        <router-link v-for="(routes,index) in links"
+        :class="{last : index === (links.length-1)}"
+        :key="routes.id" 
+        :to="`${routes.page}`">
+          {{ routes.text }}
+        </router-link>  
+      </nav> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      activeIndex: undefined,
+      links: [
+        {
+          id: 0,
+          text: 'Home',
+          page:'/home'
+        },
+        {
+          id: 1,
+          text: 'Game Lobbies',
+          page:'/gamelobbies'
+        },
+        {
+          id: 2,
+          text: 'Logout',
+          page:'/logout'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+    overflow: hidden;
+    background-color: #333;
+    padding: 0;
+    display: block;
+}
+
+.last {
+  float:right !important;
+  border-right: none !important;
+}
+
+.navbar a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    border-right: 1px solid #bbb;
+}
+
+.navbar a:hover:not(.router-link-active) {
+  background-color: #ff6500;
+  color: #FFFFFF;
+  transition: all 0.25s;
+}
+
+.router-link-active {
+  background-color: #4CAF50;
+  transition: all 0.25s;
+}
+</style>
