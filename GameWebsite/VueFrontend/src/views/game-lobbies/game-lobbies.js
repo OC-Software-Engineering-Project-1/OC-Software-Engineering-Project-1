e4c7f6a22d1f4c8ec7ebd6e34a93553df065b082
jme98 <jennifer.easter@eagles.oc.edu>
2020-05-28 18:39:47 -0500

    I think I have the file structure fixed again since my last attempt died in a dumpster fire

---
+++ GameWebsite/VueFrontend/src/views/game-lobbies/game-lobbies.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'GameLobbies',
+    data() {
+        return {
+            lobbyList: [
+                {
+                    "gameTitle": "Cryptogram Game",
+                    "occupancy": "1",
+                    "capacity": "2",
+                    "gameID": "00001",
+                    "port": "1111"
+                },
+                {
+                    "gameTitle": "Kintsugi",
+                    "occupancy": "3",
+                    "capacity": "4",
+                    "gameID": "00002",
+                    "port": "2222"
+                }
+            ]
+        }
+    }
+};
