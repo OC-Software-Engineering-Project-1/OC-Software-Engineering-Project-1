f28192f0320fd01a16d6348d74f3fd6855abbd27
JamesEHannah <35670901+JamesEHannah@users.noreply.github.com>
2020-06-07 20:14:49 -0500

    Added dropdown menu and blank pages.

--- GameWebsite/VueFrontend/src/app/app.vue
+++ GameWebsite/VueFrontend/src/app/app.vue
@@ -1,9 +1,9 @@
 <template>
 	<div id="app">
-		<div class="hidden">
-        <Navigation v-show="shouldShowNav"></Navigation>
-      </div>
-    <router-view />
+		<div>
+      <Navigation v-show="shouldShowNav"></Navigation>
+    </div>
+    <router-view/>
 	</div>
 </template>
 
