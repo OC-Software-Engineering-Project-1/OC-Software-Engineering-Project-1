54336f3729093beaab5ba4a1a02805eae06c86be
Yardomaster <51684924+Yardomaster@users.noreply.github.com>
2020-06-18 16:53:56 -0500

    Fixed merge issues and rewrote code for pulling data into game-lobbies component because it turns out route params aren't for data

--- GameWebsite/VueFrontend/src/views/game-lobbies/game-lobbies.js
+++ GameWebsite/VueFrontend/src/views/game-lobbies/game-lobbies.js
@@ -3,23 +3,46 @@ import VueJwtDecode from "vue-jwt-decode";
 
 export default {
     name: 'GameLobbies',
-    data() {
-        return {
-            gameTitle: this.$route.params.gameTitle,
-            lobbyList: Array
-        }
+    props: {
+        game: Object,
+        gameTitle: String,
+        gameURL: String,
+        gameImage: String,
+        gameDescription1: String,
+        gameDescription2: String,
+        lobbyList: Array
+    },
+    mounted() {
+        this.getGameData()
     },
     methods: {
+        async getGameData() {
+            const response = await GameService.getGame(this.gameTitle)
+            this.game = response.data
+            this.gameTitle = this.game.name
+            this.gameURL = `http://localhost:3000` + "/" + this.game.name.replace(" ", '')
+            this.gameImage = this.gameURL + "/ReferencePic.png"
+            this.gameDescription1 = this.game.description1
+            this.gameDescription2 = this.game.description2
+
+            if (this.gameDescription2.length <= 0) {
+                this.$refs.collapseButton.style.display = "none"
+            }
+        },
         async getLobbies() {
             const response = await GameService.getGameLobbies()
-            this.lobbyList = response.data.filter(function (lobby) {
-                return lobby.players.length != lobby.capacity
-            })
+            this.lobbyList = response.data
+
+            // The below code filters out full lobbies.
+            //
+            //this.lobbyList = response.data.filter(function (lobby) {
+            //    return lobby.players.length != lobby.capacity
+            //})
         },
         async createLobby() {
             const response = await GameService.getUser()
             const lobbyResponse = await GameService.createGameLobby(this.gameTitle, response.data["email"])
-            this.$router.push({ name: "game", params: { lobby: lobbyResponse, gameURL: `http://localhost:3000` + "/" + this.gameTitle } })
+            this.$router.push({ name: "game", params: { lobby: lobbyResponse, gameURL: this.gameURL } })
         },
         async joinLobby(lobby) {
             const response = await GameService.getUser()
@@ -32,7 +55,7 @@ export default {
             const lobbyResponse = await GameService.joinGameLobby(lobby._id, response.data["email"])
 
             if (lobbyResponse.data["players"].includes(response.data["email"])) {
-                this.$router.push({ name: "game", params: { lobby: lobby, gameURL: `http://localhost:3000` + "/" + this.gameTitle } })
+                this.$router.push({ name: "game", params: { lobby: lobby, gameURL: this.gameURL } })
             }
             else {
                 alert("This lobby is full. Please refresh and try again.")
