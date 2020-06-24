2e6af71085e28d17512a77caf860ebd3f8ff8781
Yardomaster <51684924+Yardomaster@users.noreply.github.com>
2020-06-23 19:51:56 -0500

    Displayed images for games page

--- GameWebsite/VueFrontend/src/views/games/games.vue
+++ GameWebsite/VueFrontend/src/views/games/games.vue
@@ -6,7 +6,7 @@
             <ul id="gameList">
                 <li v-for="(game, index) in gameList" :key="index">
                     <div class="container">
-                        <img src="./monopoly.jpg" class="gameImg">
+                        <img :src="'http://localhost:3000/' + game.name.replace(&quot; &quot;, '') + '/ReferencePic.PNG'" class="gameImg"> 
                         <router-link :to="{ name: 'gamelobbies', params: { gameTitle: game.name }}" tag="button" class="gameButton">{{ game.name }}</router-link>
                     </div>
                 </li>
