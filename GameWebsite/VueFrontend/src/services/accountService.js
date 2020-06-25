60659d346bb7047c8c5131dfc67379ecf128f61c
Blaise_Mahoro <blaise.mahoro@eagles.oc.edu>
2020-06-24 19:33:22 -0500

    Added url to image in naviagtion

--- GameWebsite/VueFrontend/src/services/accountService.js
+++ GameWebsite/VueFrontend/src/services/accountService.js
@@ -12,20 +12,24 @@ export default {
     const config = {
       headers: {
         "content-type": "multipart/form-data",
-        "Authorization": "Bearer " + localStorage.getItem("jwt"),
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
       },
     };
-    return Api().post('/users/me/avatar', formData, config);
+    return Api().post("/users/me/avatar", formData, config);
   },
-  deleteAvatar(){
+  deleteAvatar() {
     return Api().delete("/users/me/avatar", {
-        headers: { Authorization: "Bearer " + localStorage.getItem("jwt") },
-      });
+      headers: { Authorization: "Bearer " + localStorage.getItem("jwt") },
+    });
   },
-
-    updateUser(user) {
-        return Api().put('/users/me', user,{ headers: { "Authorization": "Bearer " + localStorage.getItem("jwt") } })
-    },
+  getUserAvatar() {
+    return Api().get("/users/me/avatar", {
+      headers: { Authorization: "Bearer " + localStorage.getItem("jwt") },
+    });
+  },
+  updateUser(user) {
+    return Api().put("/users/me", user, {
+      headers: { Authorization: "Bearer " + localStorage.getItem("jwt") },
+    });
+  },
 };
-    
-
